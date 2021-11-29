ce0d146782c3143a5b874a2c06c8f55986d80420
Pandaloo808 <93350605+Pandaloo808@users.noreply.github.com>
2021-11-29 16:10:16 -0500

    Update 1math.js

--- 1math.js
+++ 1math.js
@@ -1,1 +1,7 @@
-
+function addUser() {
+  player1_name=document.getElementById("player1_name_input").value;
+  player2_name=document.getElementById("player2_name_input").value;
+  LocalStorage.setItem("player1_name",player1_name);
+  LocalStorage.setItem("player2_name",player2_name);
+  window.location="2math.html";
+}
